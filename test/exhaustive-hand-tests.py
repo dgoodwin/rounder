1d0e2d81399fcadbc6a3bd5883cc29ded73f0af9
James Bowes <jbowes@dangerouslyinc.com>
2008-04-14 08:31:51 -0400

    Add running a single evaluator type, for performance tests

--- test/exhaustive-hand-tests.py
+++ test/exhaustive-hand-tests.py
@@ -4,11 +4,10 @@
 import curses
 import sys
 import random
+import time
 
-import pokereval
 
 import settestpath
-from rounder import evaluator
 
 curses.setupterm()
 color_normal = curses.tigetstr('sgr0')
@@ -68,8 +67,41 @@ def progress(total, good, match=True):
     sys.stdout.write("%s%s%s" % (color_green, msg, color_normal))
     sys.stdout.flush()
 
+def timed_progress(start_time, total):
+    current_time = time.time()
+
+    msg = "%s total tests, %0.2f ms/test (avg)" % (total,
+            (current_time - start_time)/total * 1000)
+    sys.stdout.write("\r")
+    sys.stdout.write("%s%s%s" % (color_green, msg, color_normal))
+    sys.stdout.flush()
+
+def test_one(test_type, num_players):
+    deck = make_deck()
+
+    if test_type == 'rounder':
+        from rounder.evaluator import PokerEval
+    else:
+        from pokereval import PokerEval
+
+    evaluator= PokerEval()
+    total = 0
+    start_time = time.time()
+    while True:
+        board, pockets = get_random_setup(list(deck), num_players)
+
+        winners = evaluator.winners(game="holdem",
+                pockets=pockets, board=board)['hi']
+
+        total += 1
+        timed_progress(start_time, total)
+
+
 def main(num_players):
     deck = make_deck()
+
+    import pokereval
+    from rounder import evaluator
 
     pypoker_evaluator = pokereval.PokerEval()
     rounder_evaluator = evaluator.PokerEval()
@@ -101,10 +133,22 @@ def main(num_players):
 if __name__ == "__main__":
     num_players = 2
     error_file_name = "hand-match-errors.txt"
-    if len(sys.argv) >= 2:
-        num_players = int(sys.argv[1])
-    if len(sys.argv) == 3:
-        error_file_name = sys.argv[2]
+    test_type = sys.argv[1]
+    if test_type == 'rounder':
+        pass
+    elif test_type == 'pokereval':
+        pass
+    elif test_type == 'both':
+        pass
+    else:
+        print "You must specify a test type"
+        sys.exit(1)
+
+    if len(sys.argv) >= 3:
+        num_players = int(sys.argv[2])
+    if len(sys.argv) == 4:
+        error_file_name = sys.argv[3]
+    print "testing %s" % test_type
     print "running with %s players" % num_players
     print "writing errors to %s" % error_file_name
     print "press ctrl-c to abort"
@@ -112,6 +156,9 @@ if __name__ == "__main__":
     error_file = open(error_file_name, 'w')
 
     try:
-        main(num_players)
+        if test_type == 'both':
+            main(num_players)
+        else:
+            test_one(test_type, num_players)
     except KeyboardInterrupt:
         print ""
