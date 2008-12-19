564b2508857c5ad77f7d19e0fbe7d18b78ed7e40
Steve 'Ashcrow' Milner <stevem@gnulinux.net>
2008-12-18 19:30:43 -0500

    added todo command to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,76 @@
 
 __revision__ = "$Revision$"
 
-#from setuptools import setup
-from distutils.core import setup
+
+import os
+
+from distutils.core import Command, setup
+
+
+class SetupBuildCommand(Command):
+    """
+    Master setup build command to subclass from.
+    """
+
+    user_options = []
+
+    def initialize_options(self):
+        """
+        Setup the current dir.
+        """
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        """
+        No clue ... but it's required.
+        """
+        pass
+
+
+class TODOCommand(SetupBuildCommand):
+    """
+    Quick command to show code TODO's.
+    """
+
+    description = "prints out TODO's in the code"
+
+    def run(self):
+        """
+        Prints out TODO's in the code.
+        """
+        import re
+
+        format_str = "%s (%i): %s"
+
+        # If a TODO exists, read it out
+        try:
+            line_no = 0
+            todo_obj = open('TODO', 'r')
+            for line in todo_obj.readlines():
+                print format_str % ("TODO", line_no, line[:-1])
+                line_no += 1
+            todo_obj.close()
+        except:
+            pass
+
+        remove_front_whitespace = re.compile("^[ ]*(.*)$")
+        for rootdir in ['src/', 'bin/']:
+
+            for root, dirs, files in os.walk(rootdir):
+                for afile in files:
+                    if afile[-4:] != '.pyc':
+                        full_path = os.path.join(root, afile)
+                        fobj = open(full_path, 'r')
+                        line_no = 0
+                        for line in fobj.readlines():
+                            if 'todo' in line.lower():
+                                nice_line = remove_front_whitespace.match(
+                                    line).group(1)
+                                print format_str % (full_path,
+                                                       line_no,
+                                                       nice_line)
+                            line_no += 1
+
 
 setup(name="rounder",
     version='0.0.1',
@@ -34,6 +102,8 @@ setup(name="rounder",
     # Only valid if we're using setuptools, which we're not due to problems
     # loading glade files inside eggs:
     #test_suite='runtests.suite'
+    cmdclass = {'todo': TODOCommand},
+
 )
 
 print "rounder installation complete"
